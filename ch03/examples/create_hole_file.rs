b2775ede72e46b14f9dc5b16a7acedcbffcf724a
Bob <nottxy@gmail.com>
2016-10-08 17:09:14 +0800

    ch03: Create a file with a hole in it

---
+++ ch03/examples/create_hole_file.rs
@@ -0,0 +1,36 @@
+extern crate nix;
+
+use std::io::Error;
+use std::process;
+use std::ffi::CString;
+use nix::libc::{SEEK_SET, creat, write, lseek, close, c_void};
+
+fn main() {
+    unsafe {
+        let buf1 = CString::new("abcdefghij").unwrap();
+        let buf2 = CString::new("ABCDEFGHIJ").unwrap();
+
+        let fd = creat(CString::new("file.hole").unwrap().as_ptr(), 0777);
+        if fd < 0 {
+            println!("create file error: {}", Error::last_os_error());
+            process::exit(1);
+        }
+
+        if write(fd, buf1.as_ptr() as *const c_void, 10) != 10 {
+            println!("write buf1 error: {}", Error::last_os_error());
+            process::exit(1);
+        }
+
+        if lseek(fd, 16384, SEEK_SET) == -1 {
+            println!("lseek error: {}", Error::last_os_error());
+            process::exit(1);
+        }
+
+        if write(fd, buf2.as_ptr() as *const c_void, 10) != 10 {
+            println!("write buf2 error: {}", Error::last_os_error());
+            process::exit(1);
+        }
+
+        close(fd);
+    }
+}
